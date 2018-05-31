180fc41e6fdde3d4f236acd8af974e19313781f9
BTUA\atsyhano <alexandr.tsyhanov@ua.transport.bombardier.com>
2018-05-31 13:57:35 +0300

    mouse grab added

---
+++ test_for_scene/itemhandler.cpp
@@ -0,0 +1,6 @@
+#include "itemhandler.h"
+
+ItemHandler::ItemHandler()
+{
+
+}
---
+++ test_for_scene/itemhandler.h
@@ -0,0 +1,11 @@
+#ifndef ITEMHANDLER_H
+#define ITEMHANDLER_H
+
+
+class ItemHandler
+{
+public:
+    ItemHandler();
+};
+
+#endif // ITEMHANDLER_H
